4ad8faab69f7bbe1e28497be199c08a3adac7a66
wanhaozhou <wanhao.zhou@gmail.com>
2021-12-29 16:16:51 +0100

    Update HTML title setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Contact from './components/Contact/Contact';
@@ -6,8 +6,13 @@ import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
+import { about } from './constants';
 
 const App: React.FC = () => {
+    useEffect(() => {
+        document.title = about.topic;
+    }, []);
+
     return (
         <div id="top" className={'light app'}>
             <Header />
